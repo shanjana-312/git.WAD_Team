1cc1860879bfefa102084438b4b77ae3859ed9af
Bindu Bhargavi <bindureddy1612@gmail.com>
2021-05-06 16:25:27 +0530

    minute changes to report and funds page

--- Frontend/src/components/Report.js
+++ Frontend/src/components/Report.js
@@ -5,10 +5,11 @@ function show(){
 }
 
 function Report(){
-    return(<div className="div">
+    return(
+    <div className="div">
     <form className="form-report">
             <h1>
-            REPORT
+            <u>REPORT</u>
             </h1>
             <div className="checkbox">
                     <label htmlFor="User" className="username">
@@ -18,7 +19,7 @@ function Report(){
                         type="text"
                         name="User"
                         className="User-input"
-                        placeholder="Enter username of the person.."
+                        placeholder="Enter your username"
 
                     />
 
@@ -27,13 +28,13 @@ function Report(){
             <div className="checkbox">
 
               <input type="checkbox" id="b1" name="b1" value="b1" />
-              <label>Hate Speech</label>
+              <label> Hate Speech</label>
             </div>
 
             <div className="checkbox">
 
               <input type="checkbox" id="b2" name="b2" value="b2" />
-              <label> Prank calls or mails</label>
+              <label> Prank calls\mails</label>
             </div>
 
 
@@ -47,16 +48,24 @@ function Report(){
                     <label htmlFor="Description" className="desc">
                         Description
                     </label><br></br>
-                    <textarea rows="4" cols="83"  /><br></br>
+                    <textarea rows="6" cols="83"  /><br></br>
             </div>
 
             <div className="Report">
            <button id="Report" className="report" onClick={show}>Report</button>
             </div>
+            <div className="instruction">
+              <ul>
+                <li> We will consider and try to solve your issue as soon as possible. </li>
+                <li> If your issue is not solved within a week, please contact us.</li>
+              </ul>
+            </div>
     </form>
 </div>
 
     );
+
+    
 
 
 }
