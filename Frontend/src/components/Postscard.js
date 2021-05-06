e586ce0856c0e903e2f8ef65985172f5dbf77054
anushavirri-r <anushavirri1004@gmail.com>
2021-05-06 17:08:26 +0530

    css updated

--- Frontend/src/components/Postscard.js
+++ Frontend/src/components/Postscard.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 const Postscard = ({ post }) => {
     const [click, setClick] = useState(false);
     const [click2, setClick2] = useState(false);
     const handleClick = () => setClick(!click);
     const handleClick2 = () => setClick2(!click2);
+    const [comment, setComment] = useState("");
+    const handleClick3 = e => {
+        e.preventDefault();
+        alert(" Comment Added : " + comment);
+    }
+    const handleChange = e => {
+        setComment(e.target.value);
+
+    }
+    useEffect(() => {
+
+    }, [])
     return (
         <div className="posts-cardcontainer">
             <div>
@@ -16,12 +28,14 @@ const Postscard = ({ post }) => {
                 <div className="posts-desc">
                     {post.description}
                 </div>
-                <div><i onClick={handleClick} class={click ? 'fas fa-thumbs-up' : 'far fa-thumbs-up'}  ></i> <i onClick={handleClick2} class={click2 ? 'fas fa-thumbs-down' : 'far fa-thumbs-down'}></i></div>
+                <div><i onClick={handleClick} class={click ? 'fas fa-thumbs-up' : 'far fa-thumbs-up'}  ></i>
+                    <i onClick={handleClick2} class={click2 ? 'fas fa-thumbs-down' : 'far fa-thumbs-down'}></i></div>
                 <div className="posts-comment">
-                    <input type="text" placeholder="Add a comment" className="comm" />
+                    <input type="text" name="comment" placeholder="Add a comment" value={comment} className="comm" onChange={handleChange} />
+                    <button className="addcomm" onClick={handleClick3}>+</button>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 export default Postscard;
--- Frontend/src/components/Report.js
+++ Frontend/src/components/Report.js
@@ -1,71 +1,71 @@
 import React from "react"
 import "./Report.css"
-function show(){
-   alert('Reported Successfully!!');
+function show() {
+  alert('Reported Successfully!!');
 }
 
-function Report(){
-    return(
-    <div className="div">
-    <form className="form-report">
-            <h1>
-            <u>REPORT</u>
-            </h1>
-            <div className="checkbox">
-                    <label htmlFor="User" className="username">
-                        Username
+function Report() {
+  return (
+    <div className="reportdiv">
+      <form className="form-report">
+        <h1 className="reporth1">
+          <u>REPORT</u>
+        </h1>
+        <div className="checkbox">
+          <label htmlFor="User" className="username">
+            Username
                     </label><br></br>
-                    <input id="User"
-                        type="text"
-                        name="User"
-                        className="User-input"
-                        placeholder="Enter your username"
+          <input id="User"
+            type="text"
+            name="User"
+            className="User-input"
+            placeholder="Enter your username"
 
-                    />
+          />
 
-            </div>
+        </div>
 
-            <div className="checkbox">
+        <div className="checkbox">
 
-              <input type="checkbox" id="b1" name="b1" value="b1" />
-              <label> Hate Speech</label>
-            </div>
+          <input type="checkbox" id="b1" name="b1" value="b1" />
+          <label> Hate Speech</label>
+        </div>
 
-            <div className="checkbox">
+        <div className="checkbox">
 
-              <input type="checkbox" id="b2" name="b2" value="b2" />
-              <label> Prank calls\mails</label>
-            </div>
+          <input type="checkbox" id="b2" name="b2" value="b2" />
+          <label> Prank calls\mails</label>
+        </div>
 
 
-            <div className="checkbox">
+        <div className="checkbox">
 
-              <input type="checkbox" id="b3" name="b3" value="b3" />
-              <label> Nudity</label>
-            </div>
+          <input type="checkbox" id="b3" name="b3" value="b3" />
+          <label> Nudity</label>
+        </div>
 
-            <div className="checkbox">
-                    <label htmlFor="Description" className="desc">
-                        Description
+        <div className="checkbox">
+          <label htmlFor="Description" className="desc">
+            Description
                     </label><br></br>
-                    <textarea rows="6" cols="83"  /><br></br>
-            </div>
+          <textarea rows="6" cols="83" /><br></br>
+        </div>
 
-            <div className="Report">
-           <button id="Report" className="report" onClick={show}>Report</button>
-            </div>
-            <div className="instruction">
-              <ul>
-                <li> We will consider and try to solve your issue as soon as possible. </li>
-                <li> If your issue is not solved within a week, please contact us.</li>
-              </ul>
-            </div>
-    </form>
-</div>
+        <div className="Report">
+          <button id="Report" className="report" onClick={show}>Report</button>
+        </div>
+        <div className="instruction">
+          <ul>
+            We will consider and try to solve your issue as soon as possible. <br></br>
+            If your issue is not solved within a week, please contact us.
+          </ul>
+        </div>
+      </form>
+    </div>
 
-    );
+  );
 
-    
+
 
 
 }
